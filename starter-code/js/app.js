3bbee08ace5db8af0a25d63927d69c2717e3cc14
Dan Le <Danhuyle508@gmail.com>
2018-11-08 14:46:43 -0800

    drop down bar completed

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -12,6 +12,7 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('products', JSON.stringify(Product.allProducts));
 };
 
 Cart.prototype.removeItem = function(item) {
@@ -31,6 +32,8 @@ var Product = function(filePath, name) {
   Product.allProducts.push(this);
 };
 Product.allProducts = [];
+
+
 
 function generateCatalog() {
   new Product('assets/bag.jpg', 'Bag');
